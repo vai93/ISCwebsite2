ee357f7e2d9845bd1689b2b7cf15558340d1b89b
Kaushal Ramoliya <150412945+Kaushalramoliya@users.noreply.github.com>
2025-07-13 15:30:25 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,25 @@
+const slides = document.querySelectorAll('.slide');
+let index = 0;
+
+function showSlide(i) {
+    slides.forEach((slide, idx) => {
+        slide.classList.remove('active');
+        const textBox = slide.querySelector('.text-box');
+        textBox.querySelectorAll('*').forEach(el => {
+            el.style.animation = 'none';
+            el.style.opacity = 0;
+        });
+        void textBox.offsetWidth;
+    });
+    slides[i].classList.add('active');
+    const textBox = slides[i].querySelector('.text-box');
+    textBox.querySelectorAll('*').forEach(el => {
+        el.style.animation = '';
+    });
+}
+
+setInterval(() => {
+    index = (index + 1) % slides.length;
+    showSlide(index);
+}, 4000);
+
